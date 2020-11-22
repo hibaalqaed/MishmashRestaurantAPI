dfc7d700bb416da706f33167084846aa97af98fb
HIBA ALQAED <hibaalqaed@Hibas-New-MacBook-Air.local>
2020-11-22 17:25:33 +0300

    Product Delete Route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //IMPORTS
 const express = require("express");
 const cors = require("cors");
-const products = require("./products");
+let products = require("./products");
 
 const app = express();
 
@@ -12,8 +12,23 @@ app.use(cors());
 //   response.json({ message: "Hello World" });
 // });
 
+//Routes
 app.get("/products", (req, res) => {
   res.json(products);
+});
+
+app.delete("/products/:productId", (req, res) => {
+  // const productId = req.params.productId; same as:
+  const { productId } = req.params;
+  const foundProduct = products.find((product) => product.id === +productId);
+  if (foundProduct) {
+    products = products.filter((products) => products.id !== +productId);
+    // res.status(204);
+    // res.end(); same as:
+    res.status(204).res.end();
+  } else {
+    res.status(404).json({ message: "Product not found." });
+  }
 });
 
 app.listen(8000, () => {
